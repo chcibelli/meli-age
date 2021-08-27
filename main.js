ff91f092efadffab5a5b3d7507473e45d076c4d9
Christian Cibelli <chcibelli@gmail.com>
2021-08-27 12:06:05 -0300

    disclaimer

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
 $( document ).ready(function() {
-
-
-$( "#main-btn" ).click(function() {
     
-    $('#result').hide();
-    
-    let nickname = $('#nickname').val();
-    
-    if(nickname == '') {
-        return false;
-    }
-    
-    let site = 'MLA';
-    var url = 'https://api.mercadolibre.com/sites/'+site+'/search?nickname='+nickname.trim();
-    
-    $.getJSON( url, function( data ) {
+    $( "#main-btn" ).click(function() {
         
-        if (typeof data.seller === "undefined") {
-            $('#result').html('<p>No encontre el apodo/nickname :(</p>');
+        $('#result').hide();
+        
+        let nickname = $('#nickname').val();
+        
+        if(nickname == '') {
+            $('#result').html('<p>Ingresá tu apodo/nickname :(</p>');
             $('#result').show();
-        } else {
-            var difference = getDifference(new Date(data.seller.registration_date),new Date());
-            
-            let result = '<p>Te registraste el ' + formatDate(new Date(data.seller.registration_date)) + '</p>';
-            
-            result += '<p>Estas usando Mercado Libre hace ';
-            if(difference.y >0) { result += difference.y + (difference.y > 1 ? ' años' : ' año'); }
-            if(difference.m >0) { result += ', ' + difference.m + (difference.m > 1 ? ' meses' : ' mes'); }
-            if(difference.d >0) { result += ' y ' + difference.d + (difference.d > 1 ? ' días' : ' día'); }
-            result += '</p>';
-            
-            $('#result').html(result);
-            $('#result').show();
-            
+            return false;
         }
-    });			
+        
+        let site = 'MLA';
+        let url = 'https://api.mercadolibre.com/sites/'+site+'/search?nickname='+nickname.trim();
+        
+        $.getJSON( url, function( data ) {
+            
+            if (typeof data.seller === "undefined") {
+                $('#result').html('<p>No encontre el apodo/nickname :(</p>');
+                $('#result').show();
+            } else {
+                var difference = getDifference(new Date(data.seller.registration_date),new Date());
+                
+                let result = '<p>Te registraste el ' + formatDate(new Date(data.seller.registration_date)) + '</p>';
+                
+                result += '<p>Estas usando Mercado Libre hace ';
+                if(difference.y >0) { result += difference.y + (difference.y > 1 ? ' años' : ' año'); }
+                if(difference.m >0) { result += ', ' + difference.m + (difference.m > 1 ? ' meses' : ' mes'); }
+                if(difference.d >0) { result += ' y ' + difference.d + (difference.d > 1 ? ' días' : ' día'); }
+                result += '</p>';
+                
+                $('#result').html(result);
+                $('#result').show();
+                
+            }
+        });			
+        
+        
+    });	
     
+    $("input[type=text]").keyup(function () {  
+        $(this).val($(this).val().toUpperCase());  
+    });  
     
-});	
-
-$("input[type=text]").keyup(function () {  
-    $(this).val($(this).val().toUpperCase());  
-});  
-
 });	
 
 function pad(number) {
