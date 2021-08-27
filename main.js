76257eab12f8856da91c576275f9748f0fc46040
Christian Cibelli <chcibelli@gmail.com>
2021-08-27 11:37:21 -0300

    release

---
+++ main.js
@@ -0,0 +1,66 @@
+$( document ).ready(function() {
+
+
+$( "#main-btn" ).click(function() {
+    
+    $('#result').hide();
+    
+    let nickname = $('#nickname').val();
+    
+    if(nickname == '') {
+        return false;
+    }
+    
+    let site = 'MLA';
+    var url = 'https://api.mercadolibre.com/sites/'+site+'/search?nickname='+nickname;
+    
+    $.getJSON( url, function( data ) {
+        
+        if (typeof data.seller === "undefined") {
+            $('#result').html('No encontre el apodo/nickname :( ');
+        } else {
+            var difference = getDifference(new Date(data.seller.registration_date),new Date());
+            
+            let result = 'Te registraste el ' + formatDate(new Date(data.seller.registration_date));
+            result += '<br><br>';
+            
+            result += 'Estas usando Mercado Libre hace ';
+            if(difference.y >0) { result += difference.y + (difference.y > 1 ? ' años' : ' año') + ', '; }
+            if(difference.m >0) { result += difference.m + (difference.m > 1 ? ' meses' : ' mes') + ', '; }
+            if(difference.d >0) { result += difference.d + (difference.d > 1 ? ' días' : ' día'); }
+            result += '<br><br>';
+            
+            $('#result').html(result);
+            $('#result').show();
+            
+            console.log(difference);
+        }
+        
+    });			
+    
+    
+});	
+});	
+
+function pad(number) {
+    if (number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
+
+function formatDate(date) {
+    return pad(date.getUTCDate()) + '/' + pad(date.getUTCMonth()+1) + '/' + date.getUTCFullYear();
+}
+
+function getDifference(startdate, enddate) {
+    var startdateMoment = moment(startdate);
+    var enddateMoment = moment(enddate);
+    if (startdateMoment.isValid() === true && enddateMoment.isValid() === true) {
+        var years = enddateMoment.diff(startdateMoment, 'years');				
+        var months = enddateMoment.diff(startdateMoment, 'months') - (years * 12);
+        startdateMoment.add(years, 'years').add(months, 'months');
+        var days = enddateMoment.diff(startdateMoment, 'days')
+        return {y:years, m:months, d:days};				
+    }
+}
